6e4b6fbc9dec2e43cf26fb01ba907b4e5f1305b8
Tobias Kleine <tobias.kleine@taprolabs.de>
2025-07-19 13:10:30 +0200

    linux mint setup

--- linuxmint_setup.sh
+++ linuxmint_setup.sh
@@ -8,15 +8,24 @@ sudo apt-get install gimp
 sudo apt-get install gimp-help-de
 sudo apt-get install language-pack-gnome-de
 sudo apt-get install inkscape
+sudo apt-get install chromium-browser
 
 
 
-sudo apt-get install mu-editor
-sudo apt-get install brave-browser
-
-
+# sudo apt-get install mu-editor
 
 
 flatpak install flathub com.discordapp.Discord
+
+flatpak install flathub com.visualstudio.code
 flatpak install flathub org.processing.processingide
+flatpak install flathub cc.arduino.IDE2
+flatpak install flathub com.sublimetext.three
+
 flatpak install flathub org.freecad.FreeCAD
+
+
+flatpak install flathub org.upbge.UPBGE
+flatpak install flathub org.godotengine.Godot
+
+flatpak install flathub com.brave.Browser

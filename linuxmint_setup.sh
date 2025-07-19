0b0e5ba3a64edb8e0c601781892c86b62f31780a
Tobias Kleine <tobias.kleine@taprolabs.de>
2025-07-19 13:04:52 +0200

    linux mint setup

---
+++ linuxmint_setup.sh
@@ -0,0 +1,22 @@
+
+sudo apt-get update
+sudo apt-get install curl vim net-tools apt-transport-https
+
+sudo apt-get install blender
+sudo apt-get install openscad
+sudo apt-get install gimp
+sudo apt-get install gimp-help-de
+sudo apt-get install language-pack-gnome-de
+sudo apt-get install inkscape
+
+
+
+sudo apt-get install mu-editor
+sudo apt-get install brave-browser
+
+
+
+
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub org.processing.processingide
+flatpak install flathub org.freecad.FreeCAD
